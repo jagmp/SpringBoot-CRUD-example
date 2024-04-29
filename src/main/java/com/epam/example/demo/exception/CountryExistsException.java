c45a527e04de80683deba3e6ec4e7215f8450436
Ana Pelin <creciun.a@gmail.com>
2024-04-29 14:40:51 +0300

    Solution_throw_ex_existing_country

--- src/main/java/com/epam/example/demo/exception/CountryExistsException.java
+++ src/main/java/com/epam/example/demo/exception/CountryExistsException.java
@@ -4,5 +4,6 @@ package com.epam.example.demo.exception;
 public class CountryExistsException extends RuntimeException {
     public CountryExistsException(String message) {
         super(message);
+        System.out.println("--- test");
     }
 }
